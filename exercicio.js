e7e341e628b3b6855020085787baa100f0c6b278
Yan Ribeiro <yan.ribeiro@sou.inteli.edu.br>
2025-03-17 08:52:39 -0300

    Responde a pergunta 1

---
+++ exercicio.js
@@ -0,0 +1,21 @@
+
+function analisarCredito1() {
+    var compras = [2500, 1200, 800, 100];
+    var totalCompras = compras[0];
+    var limite = 5000;
+    var status = 'aprovado';
+    var saldoDisponivel = 0;
+    var i = 1;
+
+    do {
+        totalCompras += compras[i];
+        i++;
+    } while (limite >= totalCompras && i < compras.length);
+
+    saldoDisponivel = limite - totalCompras;
+
+    if (saldoDisponivel < 0) {
+        status = 'negado';
+    }
+    console.log(`Seu crédito foi ${status}. Saldo disponível: ${saldoDisponivel}.`);
+}
